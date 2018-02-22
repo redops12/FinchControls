3e2025de5cc06ae8a310abc0c5d6dacf72450fce
unknown <rwburke@me.com>
2018-02-21 20:55:31 -0500

    tweaked stuff, have color support

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
+import javax.swing.JFrame;
 
 public class Main {
-
+	public static void main(String[] args) {
+		FinchControls f = new FinchControls();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(375,200);
+		f.setFocusable(true);
+		f.requestFocusInWindow();
+		f.setVisible(true);
+		while (true) {
+			System.out.println(f.getRed());
+		}
+	}
 }
